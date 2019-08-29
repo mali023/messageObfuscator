1969a2526edeaacfd77ecf62d58ae4e673b8af41
Muhammed Ali <mali@groworx.com>
2019-08-29 09:47:58 +0200

    Frontend

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
 //Starting express server
-    const server = app.listen(3001, () => {
+    const server = app.listen(3005, () => {
         console.log(`Express is running on port ${server.address().port}`);
     });
